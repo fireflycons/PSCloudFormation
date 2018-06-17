23c290cf7124e63729468bce3d5e464063ec3f10
FC\abm <a.m@ckay.me>
2018-06-17 14:52:46 +0100

    New function Get-StackOutputs

--- PSCloudFormation.psm1
+++ PSCloudFormation.psm1
@@ -327,7 +327,7 @@ function Update-Stack
         # Add any parameters not present on command line
         # as Use Previous Value
         $stack.Parameters |
-        ForEach-Object {
+            ForEach-Object {
 
             if ($stackParameters.ParameterKey -inotcontains $_.ParameterKey)
             {
@@ -394,10 +394,10 @@ function Update-Stack
                 Write-Host -ForegroundColor Red -BackgroundColor Black "Update failed: $arn"
                 Write-Host -ForegroundColor Red -BackgroundColor Black (
                     Get-StackFailureEvents -StackName $arn -CredentialArguments $credentialArguments | 
-                    Where-Object { $_.Timestamp -ge $updateStart } |
-                    Sort-Object -Descending Timestamp | 
-                    Out-String
-                    )
+                        Where-Object { $_.Timestamp -ge $updateStart } |
+                        Sort-Object -Descending Timestamp | 
+                        Out-String
+                )
 
                 throw $stack.StackStatusReason
             }
@@ -557,16 +557,133 @@ function Remove-Stack
                             Write-Host -ForegroundColor Red -BackgroundColor Black "$($stack.StackStatusReason)"
                             Write-Host -ForegroundColor Red -BackgroundColor Black (
                                 Get-StackFailureEvents -StackName $StackName -CredentialArguments $credentialArguments | 
-                                Where-Object { $_.Timestamp -ge $deleteBegin } |
-                                Sort-Object -Descending Timestamp | 
-                                Out-String
-                                )
+                                    Where-Object { $_.Timestamp -ge $deleteBegin } |
+                                    Sort-Object -Descending Timestamp | 
+                                    Out-String
+                            )
                         }
                         else 
                         {
                             Write-Host "Delete complete: $arn"
                         }
                     }
+                }
+            }
+        }
+    }
+}
+
+function Get-StackOutputs
+{
+    <#
+    .SYNOPSIS
+        Get the outputs of a stack in various formats
+
+    .DESCRIPTION
+        This function can be used to assist creation of new CloudFormation templates
+        that refer to the outputs of another stack.
+
+        It can be used to generate either mapping or prarameter blocks based on these outputs
+        by converting the returned object to JSON or YAML
+
+    .PARAMETER StackName
+        One or more stacks to process. One object is produced for each stack
+        
+    .PARAMETER AsMappingBlock
+        If set (default), returned object is formatted as a CloudFomration mapping block.
+        Converting the output to JSON or YAML renders text that can be pasted within a Mappings declararion.
+
+    .PARAMETER AsParameterBlock
+        If set, returned object is formatted as a CloudFormation parameter block.
+        Converting the output to JSON or YAML renders text that can be pasted within a Parameters declararion.
+
+    .PARAMETER AsCrossStackReferences
+        If set, returned object is formatted as a set of Fn::ImportValue statements, with any text matching the 
+        stack name within the output's ExportName being replaced with placeholder '${StackName}'.
+        
+        Whilst the result converted to JSON or YAML is not much use as it is, the individual elements can 
+        be copied and pasted in where an Fn::ImportValue for that parameter would be used.
+
+    .OUTPUTS
+        An object dependent on the setting of the above switches.
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'Mappings')]
+    param
+    (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Position = 0)]
+        [string[]]$StackName,
+
+        [Parameter(ParameterSetName = 'Mappings')]
+        [switch]$AsMappingBlock,
+
+        [Parameter(ParameterSetName = 'Parameters')]
+        [switch]$AsParameterBlock,
+
+        [Parameter(ParameterSetName = 'Exports')]
+        [switch]$AsCrossStackReferences
+    )
+
+    DynamicParam
+    {
+        #Create the RuntimeDefinedParameterDictionary
+        New-Object System.Management.Automation.RuntimeDefinedParameterDictionary |
+            New-CredentialDynamicParameters
+    }
+    
+    begin
+    {
+        $credentialArguments = Get-CommonCredentialParameters -CallerBoundParameters $PSBoundParameters
+    }
+
+    process
+    {
+        $StackName |
+            ForEach-Object {
+
+            if (Test-StackExists -StackName $_ -CredentialArguments $credentialArguments)
+            {
+                $outputs = @{}
+                $stack = Get-CFNStack -StackName $_ @credentialArguments
+                $stack.Outputs |
+                    ForEach-Object {
+
+                        if ($AsParameterBlock)
+                        {
+                            $param = @{
+                                Type = Get-ParameterTypeFromStringValue -Value $_.OutputValue
+                                Default = $_.OutputValue
+                            }
+
+                            if (-not [string]::IsNullOrEmpty($_.Description))
+                            {
+                                $param.Add('Description', $_.Description)
+                            }
+
+                            $outputs.Add($_.OutputKey, $param)
+                        }
+                        elseif ($AsCrossStackReferences) 
+                        {
+                            if (-not [string]::IsNullOrEmpty($_.ExportName))
+                            {
+                                $param = @{
+                                    'Fn::ImportValue' = @{
+                                        'Fn::Sub' = $_.ExportName.Replace($stack.StackName, '${StackName}')
+                                    }
+                                }
+
+                                $outputs.Add($_.OutputKey, $param)
+                            }
+                        }
+                        else
+                        {
+                            $outputs.Add($_.OutputKey, $_.OutputValue)
+                        }
+                    }
+    
+                if ($outputs.Count -gt 0)
+                {
+                    # Emit outputs as object
+                    New-Object PSObject -Property $outputs
                 }
             }
         }
@@ -848,6 +965,35 @@ function New-TemplateResolver
     $resolver
 }
 
+function Get-ParameterTypeFromStringValue
+{
+    <#
+    .SYNOPSIS
+        Determine a parameter type from a sample value
+
+    .PARAMETER Value
+        Value to test
+
+    .OUTPUTS
+        AWS parameter type, or string if a type was not matched.
+    #>
+    param
+    (
+        [string]$Value
+    )
+
+    foreach ($type in $Script:templateParameterValidators.Keys)
+    {
+        if ($Value -match $Script:templateParameterValidators[$type])
+        {
+            return $type
+        }
+    }
+
+    # No match - assume string
+    return 'String'
+}
+
 #endregion
 
 #region Dynamic Parameter magic for extracting template parameters
@@ -926,23 +1072,26 @@ function Get-CommandLineStackParameters
     }
 
     # We want this to return an array - always
-    switch(($stackParameters | Measure-Object).Count)
+    switch (($stackParameters | Measure-Object).Count)
     {
-        0 { 
+        0
+        { 
             # Stupid, stupid
             # https://stackoverflow.com/questions/18476634/powershell-doesnt-return-an-empty-array-as-an-array
             $a = @()
-            return ,$a
+            return , $a
         }
 
-        1 { 
+        1
+        { 
             # Stupid, stupid
             # https://stackoverflow.com/questions/18476634/powershell-doesnt-return-an-empty-array-as-an-array
             $a = @($stackParameters)
-            return ,$a
+            return , $a
         }
 
-        default {
+        default
+        {
             return $stackParameters
         }
     }
