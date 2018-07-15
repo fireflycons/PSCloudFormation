ef4d355544e7a05b20a82f30b7e8023df802aa5b
Alistair B. Mackay <a.m@ckay.me>
2018-07-15 16:40:46 +0100

    Start putting together a build

---
+++ build.ps1
@@ -0,0 +1,13 @@
+param ($Task = 'Default')
+
+# Grab nuget bits, install modules, set build variables, start build.
+Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+
+Install-Module Psake, PSDeploy, BuildHelpers -force -AllowClobber -Scope CurrentUser
+Install-Module Pester -MinimumVersion 4.1 -Force -AllowClobber -SkipPublisherCheck -Scope CurrentUser
+Import-Module Psake, BuildHelpers
+
+Set-BuildEnvironment -ErrorAction SilentlyContinue
+
+Invoke-psake -buildFile $ENV:BHProjectPath\psake.ps1 -taskList $Task -nologo
+exit ( [int]( -not $psake.build_success ) )
