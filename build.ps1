91de78cccb5433b23440595bbd414b3cca1727ca
eddie <a.m@ckay.me>
2018-07-16 22:32:27 +0100

    Set working directory for build.ps1

--- build.ps1
+++ build.ps1
@@ -1,13 +1,23 @@
 param ($Task = 'Default')
 
-# Grab nuget bits, install modules, set build variables, start build.
-Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+$currentLocation = Get-Location
+try
+{
+    Set-Location $PSScriptRoot
+    
+    # Grab nuget bits, install modules, set build variables, start build.
+    Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Install-Module Psake, PSDeploy, BuildHelpers -force -AllowClobber -Scope CurrentUser
-Install-Module Pester -MinimumVersion 4.1 -Force -AllowClobber -SkipPublisherCheck -Scope CurrentUser
-Import-Module Psake, BuildHelpers
+    Install-Module Psake, PSDeploy, BuildHelpers -force -AllowClobber -Scope CurrentUser
+    Install-Module Pester -MinimumVersion 4.1 -Force -AllowClobber -SkipPublisherCheck -Scope CurrentUser
+    Import-Module Psake, BuildHelpers
 
-Set-BuildEnvironment -ErrorAction SilentlyContinue
+    Set-BuildEnvironment -ErrorAction SilentlyContinue
 
-Invoke-psake -buildFile $ENV:BHProjectPath\psake.ps1 -taskList $Task -nologo
-exit ( [int]( -not $psake.build_success ) )
+    Invoke-psake -buildFile $ENV:BHProjectPath\psake.ps1 -taskList $Task -nologo
+    exit ( [int]( -not $psake.build_success ) )
+}
+finally
+{
+    Set-Location $currentLocation
+}
