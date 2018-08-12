05c85551a1bb24a41f1a0c66759d542fb4ed7fb9
Alistair Mackay <abm@firefly-consulting.co.uk>
2018-08-12 15:57:22 +0100

    Go back to forcing AWSPowerShell update

--- build.ps1
+++ build.ps1
@@ -12,6 +12,14 @@ try
     Install-Module Pester -MinimumVersion 4.1 -Force -AllowClobber -SkipPublisherCheck -Scope CurrentUser
     Import-Module Psake, BuildHelpers, platyPS
     Set-BuildEnvironment -ErrorAction SilentlyContinue
+
+    if (${env:BHBuildSystem} -eq 'AppVeyor')
+    {
+        # Seems AppVeyor's version of this is quite out of date
+        # These commands are not found: Wait-CFNStack, New-CFNChangeSet
+        Install-Module AWSPowerShell -Force -AllowClobber -SkipPublisherCheck -Scope CurrentUser
+    }
+
     Invoke-psake -buildFile $ENV:BHProjectPath\psake.ps1 -taskList $Task -nologo
     exit ( [int]( -not $psake.build_success ) )
 }
