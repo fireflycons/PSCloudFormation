e9414e400479ef8c7f2e36137a5961fcc8fe140a
Alistair Mackay <abm@firefly-consulting.co.uk>
2018-08-12 16:26:34 +0100

    Force build to fail if setup fails

--- build.ps1
+++ build.ps1
@@ -23,6 +23,13 @@ try
     Invoke-psake -buildFile $ENV:BHProjectPath\psake.ps1 -taskList $Task -nologo
     exit ( [int]( -not $psake.build_success ) )
 }
+catch
+{
+    Write-Error $_.Exception.Message
+
+    # Make AppVeyor fail the build if this setup borks
+    exit 1
+}
 finally
 {
     Set-Location $currentLocation
