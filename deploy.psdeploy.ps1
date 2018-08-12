26c8056e0bac06eea927185d518c3f875f7560a4
Alistair Mackay <abm@firefly-consulting.co.uk>
2018-08-12 11:59:55 +0100

    Set deployment to PSGallery [skip ci]

--- deploy.psdeploy.ps1
+++ deploy.psdeploy.ps1
@@ -20,7 +20,10 @@ if($ENV:BHProjectName -and $ENV:BHProjectName.Count -eq 1)
     Deploy Module {
         By PSGalleryModule {
             FromSource $ENV:BHProjectName
-            To Local
+            To PSGallery
+            WithOptions @{
+                ApiKey = $ENV:NugetApiKey
+            }
         }
     }
 }
