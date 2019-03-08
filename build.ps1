ae43025a6675227bcdb240fb030083844fabcf0b
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2019-03-08 07:14:21 +0000

    Update build logic. Update gitignore

--- build.ps1
+++ build.ps1
@@ -6,11 +6,45 @@ try
     Set-Location $PSScriptRoot
 
     # Grab nuget bits, install modules, set build variables, start build.
+    Write-Host 'Setting up build environment'
     Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-    Install-Module Psake, PSDeploy, BuildHelpers, platyPS -force -AllowClobber -Scope CurrentUser
-    Install-Module Pester -MinimumVersion 4.1 -Force -AllowClobber -SkipPublisherCheck -Scope CurrentUser
-    Import-Module Psake, BuildHelpers, platyPS
+    $loadedModules = Get-Module | Select-Object -ExpandProperty Name
+    $requiredModules = @(
+        'Psake'
+        'PSDeploy'
+        'BuildHelpers'
+        'platyPS'
+        'Pester'
+        'powershell-yaml'
+    )
+
+    # List of modules not already loaded
+    $missingModules = Compare-Object -ReferenceObject $requiredModules -DifferenceObject $loadedModules |
+    Where-Object {
+        $_.SideIndicator -eq '<='
+    } |
+    Select-Object -ExpandProperty InputObject
+
+    if ($missingModules)
+    {
+        $installedModules = Get-Module -ListAvailable | Select-Object -ExpandProperty Name
+
+        $neededModules = $requiredModules |
+            Where-Object {
+                -not ($installedModules -icontains $_)
+        }
+
+        if (($neededModules | Measure-Object).Count -gt 0)
+        {
+            Write-Host "Installing modules: $($neededModules -join ',')"
+            Install-Module $neededModules -Force -AllowClobber -SkipPublisherCheck -Scope CurrentUser
+        }
+
+        Write-Host "Importing modules: $($missingModules -join ',')"
+        Import-Module $missingModules
+    }
+
     Set-BuildEnvironment -ErrorAction SilentlyContinue
 
     if (${env:BHBuildSystem} -ine 'Unknown')
@@ -20,11 +54,6 @@ try
         Install-Module AWSPowerShell -Force -AllowClobber -SkipPublisherCheck -Scope CurrentUser
     }
 
-    if (-not (Get-Module powershell-yaml -ErrorAction SilentlyContinue))
-    {
-        Install-Module powershell-yaml -Force -AllowClobber -SkipPublisherCheck -Scope CurrentUser
-    }
-    
     Invoke-psake -buildFile $ENV:BHProjectPath\psake.ps1 -taskList $Task -nologo
     exit ( [int]( -not $psake.build_success ) )
 }
