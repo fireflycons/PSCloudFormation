4205bb677c21011e919f7f90de45c27c2d9931b7
fireflycons <34012094+fireflycons@users.noreply.github.com>
2019-10-05 08:17:41 +0100

    Supress stderr

--- cfn-flip-install.ps1
+++ cfn-flip-install.ps1
@@ -1,8 +1,8 @@
 if ((Get-Variable -Name IsLinux -ErrorAction Ignore) -and $IsLinux)
 {
-    & pip install cfn_flip --user
+    & pip install cfn_flip --user 2>&1
 }
 else
 {
-    & pip install cfn_flip
+    & pip install cfn_flip 2>&1
 }
