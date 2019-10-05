462c652b510e8cdb64373310298a308a80b6f30f
fireflycons <34012094+fireflycons@users.noreply.github.com>
2019-10-05 07:14:45 +0100

    compute output path correctly

--- PSCloudFormation/Private/Out-FileWithoutBOM.ps1
+++ PSCloudFormation/Private/Out-FileWithoutBOM.ps1
@@ -13,8 +13,21 @@ function Out-FileWithoutBOM
         [string]$FilePath
     )
 
+    # Handle difference between provider path and .NET path
+    $fullPath = $(
+        try
+        {
+            (Resolve-Path -Path $FilePath).Path
+        }
+        catch
+        {
+            New-Item -Path $FilePath -ItemType File | Out-Null
+            (Resolve-Path -Path $FilePath).Path
+        }
+    )
+
     $enc = New-Object System.Text.UTF8Encoding -ArgumentList $false
 
     # Write new manifest
-    [IO.File]::WriteAllText($FilePath, $Content, $enc)
+    [IO.File]::WriteAllText($fullPath, $Content, $enc)
 }
