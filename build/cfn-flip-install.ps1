067d6b8d2323d695be44516a7ec3cf187c30bcb4
fireflycons <34012094+fireflycons@users.noreply.github.com>
2019-10-13 10:50:54 +0100

    Check cfn-flip installation

--- build/cfn-flip-install.ps1
+++ build/cfn-flip-install.ps1
@@ -6,3 +6,9 @@ else
 {
     & pip install cfn_flip 2>&1
 }
+
+# Check installation
+if (-not (Get-Command -Name cfn-flip.exe -ErrorAction SilentlyContinue) -or (Get-Command -Name cfn-flip -ErrorAction SilentlyContinue))
+{
+    throw "Cannot find cfn-flip"
+}
