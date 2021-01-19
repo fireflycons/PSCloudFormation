e083947d43f396775bd0143f9f5a66d05d466e7e
FC\abm <34012094+fireflycons@users.noreply.github.com>
2021-01-19 20:24:12 +0000

    Add JsonConverter class Provides accurate represenstation of JSON changes in CF console

--- src/Firefly.PSCloudFormation/PSLogger.cs
+++ src/Firefly.PSCloudFormation/PSLogger.cs
@@ -7,9 +7,13 @@
     using System.Text.RegularExpressions;
 
     using Amazon.CloudFormation.Model;
+    using Amazon.Runtime;
 
     using Firefly.CloudFormation;
     using Firefly.CloudFormation.Utils;
+
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
 
     /// <summary>
     /// Concrete logger implementation for PowerShell
@@ -57,6 +61,8 @@
                 this.LogInformation("No changes to stack resources. Other changes such as Outputs may be present.");
                 return new Dictionary<string, int>();
             }
+
+            var json = JsonConvert.SerializeObject(changes.Changes, Formatting.Indented, new ConstantClassJsonConverter());
 
             var columnWidths = base.LogChangeset(changes);
             var foregroundColorMap = new Dictionary<string, ConsoleColor>
@@ -329,6 +335,60 @@
                         rawUi.WindowSize = buf;
                     }
                 }
+            }
+        }
+
+        /// <summary>
+        /// <see cref="JsonConverter"/> class to squash <see cref="ConstantClass"/> values into a <see cref="JValue"/>
+        /// </summary>
+        /// <seealso cref="Newtonsoft.Json.JsonConverter" />
+        private class ConstantClassJsonConverter : JsonConverter
+        {
+            /// <summary>
+            /// Gets a value indicating whether this <see cref="T:Newtonsoft.Json.JsonConverter" /> can read JSON.
+            /// </summary>
+            /// <value>
+            ///   <c>true</c> if this <see cref="T:Newtonsoft.Json.JsonConverter" /> can read JSON; otherwise, <c>false</c>.
+            /// </value>
+            public override bool CanRead => false;
+
+            /// <summary>
+            /// Writes the JSON representation of the object.
+            /// </summary>
+            /// <param name="writer">The <see cref="T:Newtonsoft.Json.JsonWriter" /> to write to.</param>
+            /// <param name="value">The value.</param>
+            /// <param name="serializer">The calling serializer.</param>
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                new JValue(((ConstantClass)value).Value).WriteTo(writer);
+            }
+
+            /// <summary>
+            /// Reads the JSON representation of the object.
+            /// </summary>
+            /// <param name="reader">The <see cref="T:Newtonsoft.Json.JsonReader" /> to read from.</param>
+            /// <param name="objectType">Type of the object.</param>
+            /// <param name="existingValue">The existing value of object being read.</param>
+            /// <param name="serializer">The calling serializer.</param>
+            /// <returns>
+            /// The object value.
+            /// </returns>
+            /// <exception cref="NotImplementedException">Unnecessary because CanRead is false. The type will skip the converter.</exception>
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                throw new NotImplementedException("Unnecessary because CanRead is false. The type will skip the converter.");
+            }
+
+            /// <summary>
+            /// Determines whether this instance can convert the specified object type.
+            /// </summary>
+            /// <param name="objectType">Type of the object.</param>
+            /// <returns>
+            /// <c>true</c> if this instance can convert the specified object type; otherwise, <c>false</c>.
+            /// </returns>
+            public override bool CanConvert(Type objectType)
+            {
+                return objectType.IsSubclassOf(typeof(ConstantClass));
             }
         }
     }
