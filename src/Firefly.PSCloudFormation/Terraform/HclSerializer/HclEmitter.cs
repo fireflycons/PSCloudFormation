6eab780d785526d5ace43bfbc4693f69aff2316f
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2021-12-21 07:39:33 +0000

    Tidy. Add breakpoint location on path

--- src/Firefly.PSCloudFormation/Terraform/HclSerializer/HclEmitter.cs
+++ src/Firefly.PSCloudFormation/Terraform/HclSerializer/HclEmitter.cs
@@ -340,18 +340,6 @@
             return AttributeContent.Value;
         }
 
-        private class CollectionPeeker
-        {
-            private int level;
-
-            public bool Done(HclEvent @event)
-            {
-                this.level += @event.NestingIncrease;
-
-                return this.level == 0;
-            }
-        }
-
         /// <summary>
         /// Analyzes content of a scalar.
         /// </summary>
@@ -788,6 +776,12 @@
         /// </summary>
         private void PopPath()
         {
+#if DEBUG
+            if (this.path.Count == 0)
+            {
+                // For breakpoint only
+            }
+#endif
             this.path.Pop();
         }
 
@@ -871,6 +865,31 @@
 
             this.isWhitespace = whitespace;
             this.isIndentation &= indentation;
+        }
+
+        /// <summary>
+        /// Provides a stateful predicate to <see cref="EmitterEventQueue{T}.PeekUntil"/>
+        /// to locate the end of a nested group.
+        /// </summary>
+        // ReSharper disable once StyleCop.SA1650
+        private class CollectionPeeker
+        {
+            /// <summary>
+            /// The current nesting level
+            /// </summary>
+            private int level;
+
+            /// <summary>
+            /// Determines when the end of a nested block is found in the event queue (nesting level returns to zero)
+            /// </summary>
+            /// <param name="event">The event.</param>
+            /// <returns><c>true</c> when end of nesting located.</returns>
+            public bool Done(HclEvent @event)
+            {
+                this.level += @event.NestingIncrease;
+
+                return this.level == 0;
+            }
         }
     }
 }
