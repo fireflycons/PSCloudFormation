e8f8488414443c151169153ce31fa221dc892377
Alistair Mackay <34012094+fireflycons@users.noreply.github.com>
2021-12-27 11:28:32 +0000

    Add string constants

--- src/Firefly.PSCloudFormation/Terraform/TerraformExporterConstants.cs
+++ src/Firefly.PSCloudFormation/Terraform/TerraformExporterConstants.cs
@@ -1,8 +1,15 @@
 ﻿namespace Firefly.PSCloudFormation.Terraform
 {
+    /// <summary>
+    ///  String constants that are used more than once.
+    /// </summary>
     internal static class TerraformExporterConstants
     {
         public const string AwsCloudFormationStack = "AWS::CloudFormation::Stack";
+
+        public const string AwsLambdaFunction = "AWS::Lambda::Function";
+
+        public const string LambdaZipFile = "Code.ZipFile";
 
         public const string StackOutputAttributeIndentifier = "Outputs.";
     }
